413d61490f42f904e34e5c74f30ec0ba4c79f593
Aleem Rahil <aleem.rahil@outlook.com>
2024-06-08 12:32:04 -0400

    Update query_6.sql

--- submission/query_6.sql
+++ submission/query_6.sql
@@ -1,41 +1,57 @@
-WITH
- team_games AS ( 
-   SELECT
-    a.game_id,
-    team_city,
-    game_date_est,
-    ROW_NUMBER() OVER(PARTITION BY team_city ORDER BY game_date_est) AS game_number,
-    MAX(CASE WHEN (a.team_id = b.home_team_id AND b.home_team_wins = 1) OR
-              (a.team_id = b.visitor_team_id AND b.home_team_wins = 0) THEN 1
-    ELSE 0 END)
-   AS games_won
-   FROM
-    bootcamp.nba_game_details a join bootcamp.nba_games b on a.game_id = b.game_id
-   GROUP BY
-     a.game_id,
-     team_city,
-     game_date_est
-  
-), streak AS (
-  SELECT
-   game_id,
-   team_city,
-   game_date_est,
-   -- Calculate rolling sum over a 90 game window
-   SUM(games_won) OVER(PARTITION BY team_city ORDER BY game_number
-   ROWS BETWEEN 89 PRECEDING AND CURRENT ROW) AS rolling_games_won
- FROM
-   team_games
-)
-SELECT
-    team_city,
-    MAX(rolling_games_won) AS streak_count
-  FROM
-   streak
- GROUP BY
-   1
- ORDER BY
-   2 DESC
-
-
-
+with
+    deduped_games as (
+        select
+            game_date_est,
+            home_team_id,
+            visitor_team_id,
+            home_team_wins
+        from
+            bootcamp.nba_games
+        where
+            home_team_wins is not null
+        group by
+            game_date_est,
+            home_team_id,
+            visitor_team_id,
+            home_team_wins
+    ),
+    games_both_perspectives as (
+        select
+            game_date_est,
+            home_team_id as team_id,
+            home_team_wins as is_win
+        from
+            deduped_games
+        union
+        select
+            game_date_est,
+            visitor_team_id as team_id,
+            case
+                when home_team_wins = 1 then 0
+                when home_team_wins = 0 then 1
+            end as is_win
+        from
+            deduped_games
+    ),
+    wins_over_90_games as (
+        select
+            team_id,
+            game_date_est - interval '90' game as window_start,
+            game_date_est as window_end,
+            sum(is_win) over (
+                partition by
+                    team_id
+                order by
+                    game_date_est rows between 89 preceding
+                    and current row
+            ) as n_wins
+        from
+            games_both_perspectives
+    )
+select
+    max_by(team_id, n_wins) as team_id,
+    max_by(window_start, n_wins) as window_start,
+    max_by(window_end, n_wins) as window_end,
+    max(n_wins) max_wins_over_90_games
+from
+    wins_over_90_games
